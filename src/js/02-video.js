5e365888168bf9bcb8a7bd8318b826b08c1d5485
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-19 17:00:03 +0200

    Task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const player = new Player(document.querySelector('#vimeo-player'));
+const PLAYER_KEY = "videoplayer-current-time";
+
+
+player
+    .setCurrentTime(JSON.parse(localStorage.getItem(PLAYER_KEY)))
+    .then(function (seconds) {
+
+    })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+    });
+
+player.on('timeupdate', throttle(
+    ({ seconds }) => {
+        localStorage.setItem(PLAYER_KEY, JSON.stringify(seconds));
+    }, 1000)
+);
