2f1333776d9b8e6e80b09f338c9c0071991f3ffe
Kate Panchenko <k.panchenko12345@gmail.com>
2022-12-19 16:06:26 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,8 +4,36 @@ import { galleryItems } from './gallery-items';
 
 console.log(galleryItems);
 
+import SimpleLightbox from "simplelightbox";
 
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-// import SimpleLightbox from "simplelightbox";
+const gallery = document.querySelector('.gallery');
 
-// import "simplelightbox/dist/simple-lightbox.min.css";
+const newGallery = (items) => {
+    return (
+        galleryItems
+            .map(
+                (item) =>
+                    `
+                    <a class='gallery__item' href='${item.original}'>
+                        <img
+                            class='gallery__image'
+                            src='${item.preview}'
+                            alt='${item.description}'
+                            loading='lazy'
+                        />
+                        </a>
+                                        
+                    `)
+            .join("")
+    );
+};
+
+gallery.insertAdjacentHTML("beforeend", newGallery(galleryItems));
+gallery.style.listStyle = 'none';
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
